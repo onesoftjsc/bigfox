d9ff25758e8c916d693d31cda22cc91cb0c4a646
Quan Remote <hongquantc@gmail.com>
2015-08-11 21:28:13 +0700

    add client

--- client/android/BigFoxAndroid/app/src/main/java/vn/com/onesoft/bigfox/io/message/object/UserInfo.java
+++
@@ -1,8 +0,0 @@
-package vn.com.onesoft.bigfox.io.message.object;
-
-import vn.com.onesoft.bigfox.io.message.core.annotations.Property;
-
-public class UserInfo {
-	@Property(name = "name")
-	public String name;
-}
